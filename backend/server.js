673d4cd0ecf76fd139c4ba623eb3c42838099e01
omer sharivker <sharivker90@gmail.com>
2024-05-25 14:38:42 +0300

    token,layout, display login succses or worng email/password

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const experss =require('express');
+const app=experss();
+require('dotenv').config();
+const cors=require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { dbConnect } = require('./utiles/db');
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials : true
+}))
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use('/api',require('./routes/authRoutes'));
+
+app.get('/',(req,res)=>res.send("my backend"));
+const port = process.env.PORT;
+dbConnect();
+app.listen(port, () => console.log(`Server is running on port ${port}`));
--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { admin_login, messageClear } from '../../store/Reducers/authReducer';
+import { PropagateLoader } from 'react-spinners';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 
 
+const AdminLogin = () => {
 
-const AdminLoginLogin = () => {
+     
+    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const {loader,errorMessage,successMessage}=useSelector(state=>state.auth)
+
     const [state,setState] =useState({
         email:"",
         password: ""
@@ -16,10 +26,29 @@ const AdminLoginLogin = () => {
     }
      const submit = (e)=>{
         e.preventDefault();
-        console.log(state);
+        dispatch(admin_login(state))
+
      }
     
+   const overRideStyle={
+    display :'flex',
+    margin: '0 auto',
+    height: '24px',
+    justifyContent: 'center',
+    alignItem: 'center'
+   }
 
+   useEffect(() => {
+    if (errorMessage) {
+        toast.error(errorMessage)
+        dispatch(messageClear())
+    }
+    if (successMessage) {
+      toast.success(successMessage)
+      dispatch(messageClear());
+      navigate('/');    
+  }
+},[errorMessage,successMessage])
 
     return (
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
@@ -32,7 +61,7 @@ const AdminLoginLogin = () => {
                     <img className='w-full h-full' src="http://localhost:3000/images/logo.png" alt="image"/>
                     </div>
                   </div>
-             <form onSubmit={submit}>
+                    <form onSubmit={submit}>
 
                 <div className='flex flex-col w-full gap-1 mb-3'>
                  <label htmlFor='email'>Email</label>
@@ -44,9 +73,11 @@ const AdminLoginLogin = () => {
                  <input onChange={inputHandle} value={state.password} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type='text' name='password' placeholder='Password' id='password' required />
                 </div>
 
-                <button className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Login</button>
-                
-        
+                <button disabled={loader ? true : false}  className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
+            {
+               loader ? <PropagateLoader color='#fff' cssOverride={overRideStyle} /> : 'Login'
+            } 
+            </button>
 
              </form>
 
@@ -56,4 +87,4 @@ const AdminLoginLogin = () => {
     );
 };
 
-export default AdminLoginLogin;
+export default AdminLogin;
