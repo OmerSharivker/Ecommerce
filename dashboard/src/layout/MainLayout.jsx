56cc9d8a9406aee6423414bfdf20c568dcb068d5
omer sharivker <sharivker90@gmail.com>
2024-06-01 14:13:25 +0300

    design dashboard and routing

--- dashboard/src/layout/MainLayout.jsx
+++ dashboard/src/layout/MainLayout.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 const MainLayout = () => {
+
+    const [showSideBar,setShowSideBar]=useState(false);
+
     return (
-        <div>
-            <h1>main layout <Outlet/></h1>
+        <div className='bg-[#cdcae9] w-full  min-h-screen'>
+            <Header showSideBar={showSideBar} setShowSideBar={setShowSideBar}/>
+            <Sidebar showSideBar={showSideBar} setShowSideBar={setShowSideBar}/>      
+            <div className= 'ml-0 lg:ml-[260px] pt-[95px] transition-all'> <Outlet/></div>
         </div>
     );
 };
