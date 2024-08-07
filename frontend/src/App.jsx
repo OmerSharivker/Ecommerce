26e03cdcde3926705b1b544b811c077a6dcbd2a6
Omer Sharivker <sharivker90@gmail.com>
2024-08-07 19:00:43 +0300

    design cart and shipping pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Shops from './pages/Shops';
+import Shipping from './pages/Shipping';
+import Cart from './pages/Cart';
 
 function App() {
   return (
   <BrowserRouter>
   <Routes>
     <Route path='/' element={<Home/>}  />
-
-   
+    <Route path='/shops' element={<Shops/>} />
+    <Route path='/cart' element={<Cart/>} />
+    <Route path='/shipping' element={<Shipping/>} />
   </Routes>
   
   
