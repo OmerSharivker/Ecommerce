80484a7750e087ddf1f16b806458d663bc224abe
Omer Sharivker <sharivker90@gmail.com>
2024-11-03 20:52:36 +0200

    fix

--- backend/controllers/chat/chatController.js
+++ backend/controllers/chat/chatController.js
@@ -1,9 +1,10 @@
-const sellerCustomerModel = require("../../models/chat/sellerCustomerModel")
-const customerModel = require("../../models/customerModel")
-const sellerModel = require("../../models/sellerModel")
+const sellerModel = require('../../models/sellerModel')
+const customerModel = require('../../models/customerModel')
+const sellerCustomerModel = require('../../models/chat/sellerCustomerModel')
 const sellerCustomerMessage = require('../../models/chat/sellerCustomerMessage')
+const adminSellerMessage = require('../../models/chat/adminSellerMessage')
 const { responseReturn } = require('../../utiles/response')
-const adminSellerMessage = require("../../models/chat/adminSellerMessage")
+
 
 class ChatController{
 
@@ -120,120 +121,68 @@ class ChatController{
     // End Method 
 
     customer_message_add = async (req, res) => {
-        const { userId, text, sellerId, name } = req.body;
-    
+        const {userId,text,sellerId,name } = req.body
+
         try {
-            // Create the message
             const message = await sellerCustomerMessage.create({
                 senderId: userId,
                 senderName: name,
                 receiverId: sellerId,
-                message: text,
-            });
-    
-            // Find and update customer friend list
-            const customerData = await sellerCustomerModel.findOne({ myId: userId });
-            let customerFriends = customerData?.myFriends || [];
-            if (!customerFriends.some(friend => friend.fdId === sellerId)) {
-                customerFriends.push({
-                    fdId: sellerId,
-                    name: message.senderName, // Assuming `name` is the seller's name
-                    image: "", // Add appropriate image URL if available
-                });
-                await sellerCustomerModel.updateOne(
-                    { myId: userId },
-                    { myFriends: customerFriends }
-                );
+                message : text 
+            })
+
+            const data = await sellerCustomerModel.findOne({ myId : userId })
+            let myFriends = data.myFriends
+            let index = myFriends.findIndex(f => f.fdId === sellerId)
+            while (index > 0) {
+                let temp = myFriends[index]
+                myFriends[index] = myFriends[index - 1]
+                myFriends[index - 1] = temp
+                index--
             }
-    
-            // Find and update seller friend list
-            const sellerData = await sellerCustomerModel.findOne({ myId: sellerId });
-            let sellerFriends = sellerData?.myFriends || [];
-            if (!sellerFriends.some(friend => friend.fdId === userId)) {
-                sellerFriends.push({
-                    fdId: userId,
-                    name: name, // Customer's name
-                    image: "", // Add customer’s image if available
-                });
-                await sellerCustomerModel.updateOne(
-                    { myId: sellerId },
-                    { myFriends: sellerFriends }
-                );
+            await sellerCustomerModel.updateOne(
+                {
+                    myId: userId
+                },
+                {
+                    myFriends
+                }
+
+            )
+
+
+
+            const data1 = await sellerCustomerModel.findOne({ myId : sellerId })
+            let myFriends1 = data1.myFriends
+            let index1 = myFriends1.findIndex(f => f.fdId === userId)
+            while (index1 > 0) {
+                let temp1 = myFriends1[index1]
+                myFriends1[index1] = myFriends[index1 - 1]
+                myFriends1[index1 - 1] = temp1
+                index1--
             }
-    
-            responseReturn(res, 201, { message });
+            await sellerCustomerModel.updateOne(
+                {
+                    myId: sellerId
+                },
+                {
+                    myFriends1
+                } 
+            )
+
+            responseReturn(res, 201,{message})
+
         } catch (error) {
-            console.log(error);
-            res.status(500).json({ error: "Error adding message and updating friend lists" });
+            console.log(error)
         }
-    };
-    // customer_message_add = async (req, res) => {
-    //     const {userId,text,sellerId,name } = req.body
-    //    console.log(userId,sellerId)
-    //     try {
-    //         const message = await sellerCustomerMessage.create({
-    //             senderId: userId,
-    //             senderName: name,
-    //             receiverId: sellerId,
-    //             message : text 
-    //         })
-
-    //         const data = await sellerCustomerModel.findOne({ myId : userId })
-    //         let myFriends = data.myFriends
-    //         let index = myFriends.findIndex(f => f.fdId === sellerId)
-    //         while (index > 0) {
-    //             let temp = myFriends[index]
-    //             myFriends[index] = myFriends[index - 1]
-    //             myFriends[index - 1] = temp
-    //             index--
-    //         }
-    //         await sellerCustomerModel.updateOne(
-    //             {
-    //                 myId: userId
-    //             },
-    //             {
-    //                 myFriends
-    //             }
-
-    //         )
-
-
-
-    //         const data1 = await sellerCustomerModel.findOne({ myId : sellerId })
-    //         let myFriends1 = data1.myFriends
-    //         let index1 = myFriends1.findIndex(f => f.fdId === userId)
-    //         while (index1 > 0) {
-    //             let temp1 = myFriends1[index1]
-    //             myFriends1[index1] = myFriends[index1 - 1]
-    //             myFriends1[index1 - 1] = temp1
-    //             index1--
-    //         }
-    //         await sellerCustomerModel.updateOne(
-    //             {
-    //                 myId: sellerId
-    //             },
-    //             {
-    //                 myFriends1
-    //             } 
-    //         )
-
-    //         responseReturn(res, 201,{message})
-
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
+    }
   // End Method 
 
   get_customers = async (req, res) => {
         const { sellerId } = req.params
         console.log(sellerId)
         try {
-            const data = await sellerCustomerModel.findOne({ 
-                myFriends: { 
-                    $elemMatch: { fdId: sellerId } 
-                } 
-            });
+            const data = await sellerCustomerModel.findOne({ myId : sellerId })
             responseReturn(res, 200, {
                 customers: data.myFriends
             })
@@ -286,53 +235,60 @@ class ChatController{
 
 
      seller_message_add = async (req, res) => {
-        const { senderId, receiverId, text, name } = req.body;
-    
+        const {senderId,receiverId,text,name} = req.body
         try {
-            // Create the message
             const message = await sellerCustomerMessage.create({
                 senderId: senderId,
                 senderName: name,
                 receiverId: receiverId,
-                message: text,
-            });
-    
-            // Find and update seller's friend list
-            const sellerData = await sellerCustomerModel.findOne({ myId: senderId });
-            let sellerFriends = sellerData?.myFriends || [];
-            if (!sellerFriends.some(friend => friend.fdId === receiverId)) {
-                sellerFriends.push({
-                    fdId: receiverId,
-                    name: name, // Assuming `name` is the customer's name here
-                    image: "", // Add appropriate image URL if available
-                });
-                await sellerCustomerModel.updateOne(
-                    { myId: senderId },
-                    { myFriends: sellerFriends }
-                );
+                message : text 
+            })
+
+            const data = await sellerCustomerModel.findOne({ myId : senderId })
+            let myFriends = data.myFriends
+            let index = myFriends.findIndex(f => f.fdId === receiverId)
+            while (index > 0) {
+                let temp = myFriends[index]
+                myFriends[index] = myFriends[index - 1]
+                myFriends[index - 1] = temp
+                index--
             }
-    
-            // Find and update customer's friend list
-            const customerData = await sellerCustomerModel.findOne({ myId: receiverId });
-            let customerFriends = customerData?.myFriends || [];
-            if (!customerFriends.some(friend => friend.fdId === senderId)) {
-                customerFriends.push({
-                    fdId: senderId,
-                    name: message.senderName, // Seller's name
-                    image: "", // Add seller’s image if available
-                });
-                await sellerCustomerModel.updateOne(
-                    { myId: receiverId },
-                    { myFriends: customerFriends }
-                );
+            await sellerCustomerModel.updateOne(
+                {
+                    myId: senderId
+                },
+                {
+                    myFriends
+                }
+
+            )
+
+
+
+            const data1 = await sellerCustomerModel.findOne({ myId : receiverId })
+            let myFriends1 = data1.myFriends
+            let index1 = myFriends1.findIndex(f => f.fdId === senderId)
+            while (index1 > 0) {
+                let temp1 = myFriends1[index1]
+                myFriends1[index1] = myFriends[index1 - 1]
+                myFriends1[index1 - 1] = temp1
+                index1--
             }
-    
-            responseReturn(res, 201, { message });
+            await sellerCustomerModel.updateOne(
+                {
+                    myId: receiverId
+                },
+                {
+                    myFriends1
+                } 
+            )
+
+            responseReturn(res, 201,{message})
+
         } catch (error) {
-            console.log(error);
-            res.status(500).json({ error: "Error adding message and updating friend lists" });
+            console.log(error)
         }
-    };
+     }
      // End Method 
 
 
@@ -349,12 +305,12 @@ class ChatController{
     // End Method 
 
     seller_admin_message_insert = async (req, res) => {
-        const {senderId, receiverId,message,senderName} = req.body
+        const {senderId,receiverId,message,senderName} = req.body
 
         try {
             const messageData = await adminSellerMessage.create({
                 senderId,
-                receiverId: receiverId,
+                receiverId,
                 message,
                 senderName 
             })
